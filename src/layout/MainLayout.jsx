cab75c4a8d5faaeb71c0cbaffebee16ef11b2345
JPivista <jpivistasolutions@gmail.com>
2024-08-30 17:53:43 +0530

    header changes

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
-import NavBar from "../components/headers/navbar"
+// import NavBar from "../components/headers/navbar"
 
 
 const MainLayout = () => {
     return (
         <main>
-            <NavBar />
+            {/* <NavBar /> */}
+
+            <nav>header</nav>
             <Outlet />
             <footer>Footer</footer>
         </main>
